3bfc44bf660bd173970f66252ce5f6a9cb6ddbbc
julpr1 <julprifer2@alu.edu.gva.es>
2025-10-03 12:28:23 +0200

    Add files via upload

---
+++ mamaeta.c
@@ -0,0 +1,16 @@
+/*******************************************
+ * Programa q calcula la mitjana
+ * artmetica de 2 enters
+ * Xavi Blanes PXSI 25-26 ******/
+
+#include <stdio.h>
+void main(){
+    int a, b, mitjana;
+    printf("1r núm?: ");
+    scanf("%d",&a);
+    printf("2n núm?: ");
+    scanf("%d", &b);
+    printf("La mitjana és: %d", (a+b)/2);
+    return;
+
+}
